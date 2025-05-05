8744f3bb1bbd1f9ab7ccdf56925b0cd1d316b941
hrishavvv <hrishavsanyal802@gmail.com>
2025-05-06 01:58:32 +0530

    V2

---
+++ custom.js
@@ -0,0 +1,24 @@
+window.customSongs = [
+    {
+        name: "Shape of You",
+        artists: { primary: [{ name: "Ed Sheeran" }] },
+        downloadUrl: [
+            { quality: "160kbps", url: "https://aac.saavncdn.com/126/da7cde34b008294e181842062530546d_160.mp4" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://c.saavncdn.com/126/Shape-of-You-English-2017-500x500.jpg" }
+        ]
+    },
+    {
+        name: "Attention",
+        artists: { primary: [{ name: "Charlie Puth" }] },
+        downloadUrl: [
+            { quality: "160kbps", url: "https://aac.saavncdn.com/292/3688f03d025658b1103cc8b854e1b3ed_160.mp4" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://c.saavncdn.com/292/Attention-English-2017-20190607043124-500x500.jpg" }
+        ]
+    }
+
+    
+];
